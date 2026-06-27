48ce3485760660e5466a86edf87f3f6405622265
Zenith Kandel <kandelze123@gmail.com>
2026-06-28 01:23:54 +0545

    feat: initialize new Expo project with core navigation, configuration, and essential dependencies

--- bondpay-server/src/routes/sync.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { syncBatch } = require('../controllers/syncController');
-const { authenticateToken } = require('../middleware/auth');
-
-router.post('/', authenticateToken, syncBatch);
-
-module.exports = router;
